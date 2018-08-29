e14a52c656e94a5803290745da5305ae9ff1c072
Jean-marc Lai <jeanmarc.lai@gmail.com>
2018-08-29 21:26:02 +0100

    add domain objects

---
+++ PriceCalc.Domain/Basket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace PriceCalc.Domain
+{
+    public class Basket
+    {
+        private List<Product> _basket = new List<Product>();
+        private OfferManager _offerManager;
+
+        public Basket(OfferManager offerManager)
+        {
+            _offerManager = offerManager;
+        }
+
+        public void Add(Product product)
+        {
+            _basket.Add(product);
+        }
+
+        private void ApplyOffers()
+        {
+            var offers = new List<Offer>();
+            _basket.RemoveAll(_ => _.GetType() == typeof(Offer));
+            _offerManager.Offers.ForEach(_ => offers.Add(_.Invoke(_basket)));
+            _basket.AddRange(offers);
+        }
+
+        public decimal Total()
+        {
+            ApplyOffers();
+            decimal total = 0;
+            _basket.ForEach(_ => total += _.Price);
+            return total;
+        }
+    }
+}
---
+++ PriceCalc.Domain/OfferManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PriceCalc.Domain
+{
+    public class OfferManager
+    {        
+        public List<Func<IEnumerable<Product>, Offer>> Offers { get; private set; }
+
+        private void BreadOffer()
+        {
+            Offers.Add(
+            (products) => {
+
+                var bread = products.Where(_ => _.Id == 3);
+                if (bread == null) return null;
+
+                return new Offer
+                {
+                    Id = 4,
+                    Name = "Buy 2 Butter and get a Bread at 50% off",
+                    Price = -1 * bread.First().Price / 2M *
+                    Math.Min(
+                        products.Count(_ => _.Id == 1) / 2,
+                        products.Count(_ => _.Id == 3)
+                    )
+                };
+            });
+        }
+
+        private void MilkOffer()
+        {
+            Offers.Add(
+            (products) =>
+            {
+
+                var milk = products.Where(_ => _.Id == 2);
+                if (milk == null) return null;
+
+                return new Offer
+                {
+                    Id = 4,
+                    Name = "Buy 3 Milk and get the 4th milk for free",
+                    Price = -1 * milk.First().Price * milk.Count() / 4
+                };
+            });
+        }
+    }
+}
